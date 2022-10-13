b3a7ebbb8b4143c3c38d745fc88900c315027e3e
JAston <jacob.aston1@gmail.com>
2022-10-12 21:47:27 -0600

    Pseudo time blocks display for the number of hours

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const $currentDay = $('#currentDay');
 const $container = $('div')
+var now = dayjs();
+const hours = ['9','10','11','12','1','2','3','4','5'];
+
+
+
 $currentDay.text(now);
 
-var now = dayjs();
-
 const loadTimeBlocks = () => {
-    var timeBlock = "<div class ='time-block'>howdy2</div>"
-    $container.append("<div>howdy</div>");
-    $container.append(timeBlock);
+    var timeBlock = "<div class ='time-block row'>howdy2</div>"
 
+    hours.forEach(function(){
+        $container.append(timeBlock);
+
+    })
 }
 
 loadTimeBlocks()
