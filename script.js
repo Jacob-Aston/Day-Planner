0336dec01d4310ae89c88c9ed609ac3ace0b98e3
JAston <jacob.aston1@gmail.com>
2022-10-13 15:42:37 -0600

    Changed hours array into array of objects.

--- script.js
+++ script.js
@@ -2,13 +2,52 @@ const $currentDay = $("#currentDay");
 const $container = $("div");
 const $timeBlocks = $("div.container > div");
 var now = dayjs();
-const hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+const hours = [{
+    hour : "9am",
+    description : "",
+    time : ""
+}, {
+    hour : "10am",
+    description : "",
+    time : ""
+}, {
+    hour : "11am",
+    description : "",
+    time : ""
+}, {
+    hour : "12am",
+    description : "",
+    time : ""
+}, {
+    hour : "1pm",
+    description : "",
+    time : ""
+}, {
+    hour : "2pm",
+    description : "",
+    time : ""
+}, {
+    hour : "3pm",
+    description : "",
+    time : ""
+}, {
+    hour : "4pm",
+    description : "",
+    time : ""
+}, {
+    hour : "5pm",
+    description : "",
+    time : ""
+}]
+
+    
+//    , "10", "11", "12", "1", "2", "3", "4", "5"];
 
 $currentDay.text(now);
 
 const loadTimeBlocks = () => {
   for (i = 0; i < hours.length; i++) {
-    var timeBlock = $('<div class ="time-block row">' + '<div class="hour col-2">' + hours[i] + '</div>' + '<input class="description col-8"></input>' + '<btn class="saveBtn btn col-2">click</btn>' + '</div>');
+    var timeBlock = $('<div class ="time-block row">' + '<div class="hour col-2">' + hours[i].hour + '</div>' + '<input class="description col-8">' + hours[i].description + '</input>' + '<btn class="saveBtn btn col-2">click</btn>' + '</div>');
     $container.append(timeBlock);
 }
 };
