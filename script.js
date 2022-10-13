ed30c81f5a858fed6583b8880884359c748c753d
JAston <jacob.aston1@gmail.com>
2022-10-13 15:28:47 -0600

    Timeblocks display with bootstrap styles.

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-const $currentDay = $('#currentDay');
-const $container = $('div')
+const $currentDay = $("#currentDay");
+const $container = $("div");
+const $timeBlocks = $("div.container > div");
 var now = dayjs();
-const hours = ['9','10','11','12','1','2','3','4','5'];
-
-
+const hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
 
 $currentDay.text(now);
 
 const loadTimeBlocks = () => {
-    for (i = 0; i < hours.length; i++) {
-    var timeBlock = $('<div class ="time-block row">'+ hours[i] + '</div>')
+  for (i = 0; i < hours.length; i++) {
+    var timeBlock = $('<div class ="time-block row">' + '<div class="hour col-2">' + hours[i] + '</div>' + '<input class="description col-8"></input>' + '<btn class="saveBtn btn col-2">click</btn>' + '</div>');
     $container.append(timeBlock);
-    }
 }
+};
 
-const fillTimeBlocks = () => {
-    const $timeBlocks = $("div.container > div")
-    $timeBlocks.array.forEach(element => {
-        var timeFill = $('<div class="hour"></div>')
-        element.append(timeFill)
-    });
-}
-
-
-
-loadTimeBlocks()
-fillTimeBlocks()
+loadTimeBlocks();
