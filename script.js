ca2397417403311711fda20e00aa3568bc6e3c62
JAston <jacob.aston1@gmail.com>
2022-10-17 00:40:47 -0600

    Reworked Javascript and finished project.

--- script.js
+++ script.js
@@ -1,109 +1,80 @@
 const $currentDay = $("#currentDay");
-const $container = $("div");
-const $timeBlocks = $("div.container > div");
-var now = dayjs().format("dddd-MMM-DD-YYYY");
-var currentHour = dayjs().hour();
-console.log(currentHour);
+const $container = $("div.container");
+const $timeBlocks = $(".time-block");
+const nowDisplay = dayjs().format("dddd, MMM DD YYYY");
+const currentHour = dayjs().hour();
 
-const hours = [
-  {
-    hour: "9am",
-    description: "",
-    time: "9",
+const hoursApi = {
+  getHours: () => {
+    const hours = window.localStorage.getItem('hours');
+    return JSON.parse(hours);
   },
-  {
-    hour: "10am",
-    description: "",
-    time: "10",
-  },
-  {
-    hour: "11am",
-    description: "",
-    time: "11",
-  },
-  {
-    hour: "12am",
-    description: "",
-    time: "12",
-  },
-  {
-    hour: "1pm",
-    description: "",
-    time: "13",
-  },
-  {
-    hour: "2pm",
-    description: "",
-    time: "14",
-  },
-  {
-    hour: "3pm",
-    description: "",
-    time: "15",
-  },
-  {
-    hour: "4pm",
-    description: "yuh",
-    time: "16",
-  },
-  {
-    hour: "5pm",
-    description: "",
-    time: "17",
-  },
-];
+  setHours: (hours) => {
+    window.localStorage.setItem('hours', JSON.stringify(hours))
+  }
+}
 
-$currentDay.text(now);
+let hours = hoursApi.getHours()
+if (!hours) {
+  const hoursInDay = [9,10,11,12,13,14,15,16,17];
+  const newHours = hoursInDay.map((hour) => {
+    const baseTwelveHour = hour % 12;
+    return {
+      id: hour,
+      label: `${baseTwelveHour === 0 ? 12 : baseTwelveHour}${ hour < 12 ? 'am' : 'pm'}`,
+      description: '',
+      time: hour
+    }
+  })
+  hoursApi.setHours(newHours)
+  hours = newHours
+}
 
-const hourCheck = (i) => {
-  var hour = hours[i].time;
-  console.log(+hour);
-  if (hour == currentHour) {
+$currentDay.text(nowDisplay);
+
+const hourCheck = (index) => {
+  const {time} = hours[index];
+  if (time == currentHour) {
     return "present";
   }
-  if (hour > currentHour) {
+  if (time > currentHour) {
     return "future";
   }
-  if (hour < currentHour) {
+  if (time < currentHour) {
     return "past";
   }
 };
 
+const save = (event) => {
+  event.preventDefault();
+  // The id of the form is the id of the hour object it's related to
+  const hourId = parseInt(event.target.id);
+  const newDescription = $(`#${hourId} textarea.description`).val();
+
+  // Copy our array
+  const updatedHours = hours.slice();
+  const hourToUpdateIndex = updatedHours.findIndex(hour => hour.id === hourId);
+  updatedHours[hourToUpdateIndex].description = newDescription;
+
+  hoursApi.setHours(updatedHours);
+}
+
 const loadTimeBlocks = () => {
-  for (i = 0; i < hours.length; i++) {
-    var timeBlock = $(
-      '<div class ="time-block row">' +
-        '<div class="hour col-2">' +
-        hours[i].hour +
-        "</div>" +
-        '<textarea class="description col-8 ' +
-        hourCheck(i) +
-        '" readonly>' +
-        hours[i].description +
-        "</textarea>" +
-        '<btn class="saveBtn btn col-2">save</btn>' +
-        "</div>"
-    );
+  hours.forEach(({label, description, id}, index) => {
+    const timeBlock = $(`
+      <div class="time-block row">
+        <div class="hour col-2">
+          ${label}
+        </div>
+        <form class="row col-10" id="${id}">
+          <textarea class="description col-9 ${hourCheck(index)}" name="description" value="${description}" />
+          <button type="submit" class="saveBtn btn col-3">save</button>
+        </form>
+      </div>
+    `)
     $container.append(timeBlock);
-  }
+    $(`#${id}`).submit(save)
+  })
 };
 
 loadTimeBlocks();
-
-// //https://jsfiddle.net/GeJkU/
-// const divClicked = () => {
-//     var divHtml = $(this).html();
-//     var editableText = $("<textarea /");
-//     editableText.val(divHtml);
-//     $(this).replaceWith(editableText);
-//     editableText.focus();
-//     editableText.blur(editableTextBlurred)
-// }
-// const editableTextBlurred = () => {
-//     var html = $(this).val();
-//     var viewableText = $("<div>");
-//     viewableText.html(html);
-//     $(this).replaaceWith(viewableText);
-//     $(viewableText).click(divClicked);
-// }
-
